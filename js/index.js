2c4b02ed85faba51905b1252dc143c17f8b3a77b
aishsingh <a@aishsingh.com>
2017-07-09 18:34:51 +1000

    Added animations

--- js/index.js
+++ js/index.js
@@ -28,9 +28,13 @@ function init() {
         
     //TODO Add delay
     Velocity(document.getElementById("bg-tint"), { opacity: 0.7 }, { duration: 5000 });
-    Velocity(document.getElementById("logo"), { opacity: 1.0 }, { duration: 1500 });
-    Velocity(document.getElementById("locations"), { opacity: 1.0 }, { duration: 3000 });
-    Velocity(document.getElementById("nav-menu"), { opacity: 1.0 }, { duration: 3000 });
+    Velocity(document.getElementById("logo"), { opacity: 1.0 }, { delay: 2400, duration: 2200 });
+    Velocity(document.getElementById("locations"), { translateY: -100}, { duration: 0 });
+    Velocity(document.getElementById("locations"), { translateY: 0, opacity: 1.0 }, { delay: 3100, duration: 1500 });
+    Velocity(document.getElementById("nav-menu"), { translateX: "-50%", translateY: 50 }, { duration: 0 });
+    Velocity(document.getElementById("nav-menu"), { translateX: "-50%", translateY: 0, opacity: 1.0 }, { delay: 3100, duration: 1500 });
+    Velocity(document.getElementById("bg"), { scaleX: 10, scaleY: 10, translateY: -220 }, { duration: 0 });
+    Velocity(document.getElementById("bg"), { scaleX: 1.1, scaleY: 1.1 , translateY: 0}, { duration: 3000 });
 }
 
 function mousemove(e) {
@@ -40,13 +44,13 @@ function mousemove(e) {
     // var bgImgHeight = img.height;
 
     // BG
-    var mag = 6;  // Magnitude of change
-    // var xpos = (window.innerWidth/(mag*2)) - e.clientX/mag;
-    var xpos = ((window.innerWidth/(mag*2)) - (100)) - e.clientX/mag;
+    var mag = 20;  // Magnitude of change
+    var xpos = (window.innerWidth/(mag*2)) - e.clientX/mag;
+    //var xpos = ((window.innerWidth/(mag*2)) - (100)) - e.clientX/mag;
     var ypos = (window.innerHeight/(mag*2)) - e.clientY/mag;
 
     // Velocity(document.getElementById("bg"), { backgroundOriginX: xpos, backgroundOriginY: ypos }, { duration: 0 });
-    document.getElementById("bg").style.backgroundPosition = xpos.toString() + "px " + ypos.toString() + "px";
+    // document.getElementById("bg").style.backgroundPosition = xpos.toString() + "px " + ypos.toString() + "px";
 
     // Logo element
     // mag = 15;  // Magnitude of change
@@ -67,7 +71,16 @@ function mousemove(e) {
     // xpos = ((window.innerWidth/(mag*2)) - (document.getElementById("nav-menu").width/2)) - e.clientX/mag;
     // ypos = ((window.innerHeight/(mag*2)) - (document.getElementById("nav-menu").height/2)) - e.clientY/mag;
     // Velocity(document.getElementById("nav-menu"), { translateX: xpos, translateY: ypos }, { duration: 0 });
+    
 
+
+    // $.Velocity.RegisterEffect("callout.pulse", {
+    //     defaultDuration: 900,
+    //     calls: [
+    //         [ { scaleX: 1.1 }, 0.50 ],
+    //         [ { scaleX: 1 }, 0.50 ]
+    //     ]
+    // })
 }
 
 window.onload = init;
