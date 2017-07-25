c3c06e76ef0259939c8231f6b8fec9ac40e0da90
aishsingh <a@aishsingh.com>
2017-07-26 03:43:26 +1000

    Before changing layout

--- js/index.js
+++ js/index.js
@@ -24,17 +24,23 @@ function init() {
     document.body.addEventListener("mousemove", function(event) {
         mousemove(event);
     });
+    // document.body.addEventListener("mouseenter", function(event) {
+    //     mousemove(event);
+    // });
 
         
     //TODO Add delay
-    Velocity(document.getElementById("bg-tint"), { opacity: 0.7 }, { duration: 5000 });
-    Velocity(document.getElementById("logo"), { opacity: 1.0 }, { delay: 2400, duration: 2200 });
+    Velocity(document.getElementById("bg"), { opacity: 1 }, { duration: 200 });
+    Velocity(document.getElementById("bg-tint"), { opacity: 0.0 }, { duration: 1250 });
+    Velocity(document.getElementById("logo"), { opacity: 1.0 }, { delay: 1200, duration: 2200 });
     Velocity(document.getElementById("locations"), { translateY: -100}, { duration: 0 });
     Velocity(document.getElementById("locations"), { translateY: 0, opacity: 1.0 }, { delay: 3100, duration: 1500 });
-    Velocity(document.getElementById("nav-menu"), { translateX: "-50%", translateY: 50 }, { duration: 0 });
+    Velocity(document.getElementById("nav-menu"), { translateX: "-50%", translateY: 50 }, { stagger: 500, duration: 0 });
     Velocity(document.getElementById("nav-menu"), { translateX: "-50%", translateY: 0, opacity: 1.0 }, { delay: 3100, duration: 1500 });
-    Velocity(document.getElementById("bg"), { scaleX: 10, scaleY: 10, translateY: -220 }, { duration: 0 });
-    Velocity(document.getElementById("bg"), { scaleX: 1.1, scaleY: 1.1 , translateY: 0}, { duration: 3000 });
+    Velocity(document.getElementById("bg"), { scaleX: 0.8, scaleY: 0.8, translateY: 150 }, { duration: 0 });
+    Velocity(document.getElementById("bg"), { scaleX: 1.1, scaleY: 1.1 , translateY: 0}, { duration: 3200, easing: "easeInBack" });
+    // Velocity(document.getElementById("bg"), { translateY: 1220 }, { duration: 0 });
+    // Velocity(document.getElementById("bg"), { translateY: 0}, { duration: 4000, easing: "easeInBack" });
 }
 
 function mousemove(e) {
@@ -45,12 +51,12 @@ function mousemove(e) {
 
     // BG
     var mag = 20;  // Magnitude of change
-    var xpos = (window.innerWidth/(mag*2)) - e.clientX/mag;
-    //var xpos = ((window.innerWidth/(mag*2)) - (100)) - e.clientX/mag;
-    var ypos = (window.innerHeight/(mag*2)) - e.clientY/mag;
+    //var xpos = (window.innerWidth/(mag*2)) - e.clientX/mag;
+    var xpos = ((window.innerWidth/(mag*2)) - (-52)) - e.clientX/mag;
+    var ypos = ((window.innerHeight/(mag*2)) - (52)) - e.clientY/mag;
 
     // Velocity(document.getElementById("bg"), { backgroundOriginX: xpos, backgroundOriginY: ypos }, { duration: 0 });
-    // document.getElementById("bg").style.backgroundPosition = xpos.toString() + "px " + ypos.toString() + "px";
+    document.getElementById("bg").style.backgroundPosition = xpos.toString() + "px " + ypos.toString() + "px";
 
     // Logo element
     // mag = 15;  // Magnitude of change
@@ -71,16 +77,6 @@ function mousemove(e) {
     // xpos = ((window.innerWidth/(mag*2)) - (document.getElementById("nav-menu").width/2)) - e.clientX/mag;
     // ypos = ((window.innerHeight/(mag*2)) - (document.getElementById("nav-menu").height/2)) - e.clientY/mag;
     // Velocity(document.getElementById("nav-menu"), { translateX: xpos, translateY: ypos }, { duration: 0 });
-    
-
-
-    // $.Velocity.RegisterEffect("callout.pulse", {
-    //     defaultDuration: 900,
-    //     calls: [
-    //         [ { scaleX: 1.1 }, 0.50 ],
-    //         [ { scaleX: 1 }, 0.50 ]
-    //     ]
-    // })
 }
 
 window.onload = init;
