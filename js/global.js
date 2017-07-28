e2332a4a98d9d67d537c7799371a09b167c1dcb3
aishsingh <a@aishsingh.com>
2017-07-28 14:07:20 +1000

    Added home to navmenu, fixed meta desc

--- js/global.js
+++ js/global.js
@@ -3,6 +3,19 @@ function init() {
     Velocity(document.getElementById("nav-menu"), { translateX: "-50%", translateY: 0, opacity: 1.0 }, { duration: 500 });
 }
 
+function hoverLogo() {
+    document.getElementById("ic_logo").src='images/logo_icon_colour.png';
+    // Velocity(document.getElementById("nav_food"), { color: '#efcc82' }, { duration: 0 });
+    Velocity(document.getElementById("nav_logo"), { fontSize: 32 }, { duration: 300 });
+    Velocity(document.getElementById("ic_logo"), { width: 55 }, { duration: 300 });
+}
+function leaveLogo() {
+    document.getElementById("ic_logo").src='images/logo_icon.png';
+    // Velocity(document.getElementById("nav_food"), { color: '#000' }, { duration: 0 });
+    Velocity(document.getElementById("nav_logo"), { fontSize: 25 }, { duration: 200 });
+    Velocity(document.getElementById("ic_logo"), { width: 50 }, { duration: 200 });
+}
+
 function hoverFood() {
     document.getElementById("ic_food").src='images/Cutlery_colour.png';
     // Velocity(document.getElementById("nav_food"), { color: '#efcc82' }, { duration: 0 });
