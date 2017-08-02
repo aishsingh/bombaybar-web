d532ae7f3db5ff03b6e4bcc64140d13e03e6e691
aishsingh <a@aishsingh.com>
2017-08-02 15:05:33 +1000

    Added gradient tint to slideshow

--- js/index.js
+++ js/index.js
@@ -1,52 +1,82 @@
 var next_slide;
+var slide_timer;
+var slide_interval;
 function startSlideShow() {
+    Velocity(document.getElementById("bg-tint"), { opacity: 1.0 }, { duration: 1000 });
+
     next_slide = 1;
+    slide_interval = 5000;
     changeSlide(next_slide);
-    setInterval(function(){changeSlide(next_slide)}, 5000);
+    slide_timer = setInterval(function(){changeSlide(next_slide)}, slide_interval);
 }
 
 function changeSlide(slide) {
     if (slide == 1) {
-        Velocity(document.getElementById("bg-tint"), { opacity: 0.25 }, { duration: 1000 });
         Velocity(document.getElementById("logo"), { opacity: 1.0 }, { duration: 1000 });
         Velocity(document.getElementById("slide1"), { opacity: 1.0 }, { duration: 1000 });
         Velocity(document.getElementById("slide2"), { opacity: 0.0 }, { duration: 1000 });
         Velocity(document.getElementById("slide3"), { opacity: 0.0 }, { duration: 1000 });
-        Velocity(document.getElementById("logo"), { opacity: 0.0 }, { delay: 3000, duration: 1000 });
-        Velocity(document.getElementById("loc_emporium"), { opacity: 1.0 }, { duration: 1000 });
-        Velocity(document.getElementById("loc_westfield"), { opacity: 1.0 }, { duration: 1000 });
         Velocity(document.getElementById("slide_indicator1"), { backgroundColor: '#000' }, { duration: 1000 });
         Velocity(document.getElementById("slide_indicator2"), { backgroundColor: '#FFF' }, { duration: 1000 });
         Velocity(document.getElementById("slide_indicator3"), { backgroundColor: '#FFF' }, { duration: 1000 });
-        Velocity(document.getElementById("bg-tint"), { opacity: 0 }, { delay: 3000, duration: 1000 });
+
+        Velocity(document.getElementById("loc_emporium"), { opacity: 1.0 }, { duration: 1000 });
+        Velocity(document.getElementById("loc_westfield"), { opacity: 1.0 }, { duration: 1000 });
         next_slide = 2;
     } else if (slide == 2) {
-        Velocity(document.getElementById("slide1"), { opacity: 0.0 }, { duration: 1000 });
-        Velocity(document.getElementById("slide2"), { opacity: 1.0 }, { duration: 1000 });
-        Velocity(document.getElementById("slide3"), { opacity: 0.0 }, { duration: 1000 });
-        Velocity(document.getElementById("logo"), { opacity: 0.0 }, { duration: 1000 });
+        if (parseFloat(document.getElementById("logo").style.opacity) > 0) {
+            Velocity(document.getElementById("logo"), { opacity: 0.0 }, { duration: 500 });
+            Velocity(document.getElementById("slide1"), { opacity: 0.0 }, { delay: 500, duration: 1000 });
+            Velocity(document.getElementById("slide2"), { opacity: 1.0 }, { delay: 500, duration: 1000 });
+            Velocity(document.getElementById("slide3"), { opacity: 0.0 }, { delay: 500, duration: 1000 });
+            Velocity(document.getElementById("slide_indicator1"), { backgroundColor: '#FFF' }, { delay: 500, duration: 1000 });
+            Velocity(document.getElementById("slide_indicator2"), { backgroundColor: '#000' }, { delay: 500, duration: 1000 });
+            Velocity(document.getElementById("slide_indicator3"), { backgroundColor: '#FFF' }, { delay: 500, duration: 1000 });
+        }
+        else {
+            Velocity(document.getElementById("slide1"), { opacity: 0.0 }, { duration: 1000 });
+            Velocity(document.getElementById("slide2"), { opacity: 1.0 }, { duration: 1000 });
+            Velocity(document.getElementById("slide3"), { opacity: 0.0 }, { duration: 1000 });
+            Velocity(document.getElementById("slide_indicator1"), { backgroundColor: '#FFF' }, { duration: 1000 });
+            Velocity(document.getElementById("slide_indicator2"), { backgroundColor: '#000' }, { duration: 1000 });
+            Velocity(document.getElementById("slide_indicator3"), { backgroundColor: '#FFF' }, { duration: 1000 });
+        }
+
         Velocity(document.getElementById("loc_emporium"), { opacity: 1.0 }, { duration: 1000 });
-        Velocity(document.getElementById("loc_westfield"), { opacity: 0.0 }, { duration: 1000 });
-        Velocity(document.getElementById("slide_indicator1"), { backgroundColor: '#FFF' }, { duration: 1000 });
-        Velocity(document.getElementById("slide_indicator2"), { backgroundColor: '#000' }, { duration: 1000 });
-        Velocity(document.getElementById("slide_indicator3"), { backgroundColor: '#FFF' }, { duration: 1000 });
+        Velocity(document.getElementById("loc_westfield"), { opacity: 0.3 }, { duration: 1000 });
         next_slide = 3;
     } else if (slide == 3) {
-        Velocity(document.getElementById("slide1"), { opacity: 0.0 }, { duration: 1000 });
-        Velocity(document.getElementById("slide2"), { opacity: 0.0 }, { duration: 1000 });
-        Velocity(document.getElementById("slide3"), { opacity: 1.0 }, { duration: 1000 });
-        Velocity(document.getElementById("loc_emporium"), { opacity: 0.0 }, { duration: 1000 });
-        Velocity(document.getElementById("loc_westfield"), { translateY: -75 }, { duration: 0 });
-        Velocity(document.getElementById("loc_westfield"), { translateY: -75, opacity: 1.0 }, { duration: 1000 });
-        Velocity(document.getElementById("slide_indicator1"), { backgroundColor: '#FFF' }, { duration: 1000 });
-        Velocity(document.getElementById("slide_indicator2"), { backgroundColor: '#FFF' }, { duration: 1000 });
-        Velocity(document.getElementById("slide_indicator3"), { backgroundColor: '#000' }, { duration: 1000 });
-        Velocity(document.getElementById("loc_westfield"), { translateY: 0.0 }, { delay: 3000, duration: 1000 });
-        // Velocity(document.getElementById("loc_emporium"), { opacity: 0.0 }, { duration: 1000 });
-        // Velocity(document.getElementById("loc_westfield"), { opacity: 0.0 }, { duration: 1000 });
+        if (parseFloat(document.getElementById("logo").style.opacity) > 0) {
+            Velocity(document.getElementById("logo"), { opacity: 0.0 }, { duration: 500 });
+            Velocity(document.getElementById("slide1"), { opacity: 0.0 }, { delay: 500, duration: 1000 });
+            Velocity(document.getElementById("slide2"), { opacity: 0.0 }, { delay: 500, duration: 1000 });
+            Velocity(document.getElementById("slide3"), { opacity: 1.0 }, { delay: 500, duration: 1000 });
+            Velocity(document.getElementById("slide_indicator1"), { backgroundColor: '#FFF' }, { delay: 500, duration: 1000 });
+            Velocity(document.getElementById("slide_indicator2"), { backgroundColor: '#FFF' }, { delay: 500, duration: 1000 });
+            Velocity(document.getElementById("slide_indicator3"), { backgroundColor: '#000' }, { delay: 500, duration: 1000 });
+        }
+        else {
+            Velocity(document.getElementById("slide1"), { opacity: 0.0 }, { duration: 1000 });
+            Velocity(document.getElementById("slide2"), { opacity: 0.0 }, { duration: 1000 });
+            Velocity(document.getElementById("slide3"), { opacity: 1.0 }, { duration: 1000 });
+            Velocity(document.getElementById("slide_indicator1"), { backgroundColor: '#FFF' }, { duration: 1000 });
+            Velocity(document.getElementById("slide_indicator2"), { backgroundColor: '#FFF' }, { duration: 1000 });
+            Velocity(document.getElementById("slide_indicator3"), { backgroundColor: '#000' }, { duration: 1000 });
+        }
+
+
+        Velocity(document.getElementById("loc_emporium"), { opacity: 0.3 }, { duration: 1000 });
+        Velocity(document.getElementById("loc_westfield"), { opacity: 1.0 }, { duration: 1000 });
         next_slide = 1;
     }
 
+}
+
+function resetToSlide(slide) {
+    changeSlide(slide);
+    clearInterval(slide_timer);
+    slide_timer = null;
+    slide_timer = setInterval(function(){changeSlide(next_slide)}, slide_interval);
 }
 
 window.addEventListener("load", startSlideShow, true);
